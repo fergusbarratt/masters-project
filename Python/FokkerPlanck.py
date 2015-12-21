32691ad438bbda98cdd312e273b376ea7b01387e
Fergus Barratt <fergus.barratt.12@ucl.ac.uk>
2015-12-21 15:16:17 +0000

    more fokker planck sim

--- Python/FokkerPlanck.py
+++ Python/FokkerPlanck.py
@@ -3,36 +3,46 @@
 import matplotlib
 matplotlib.use('TkAgg')
 import numpy as np
+from numpy import Inf
 from scipy.linalg import expm
+from scipy.integrate import quad as integr
 import matplotlib.pyplot as plt
 
 
-def diffuse(A, D, initial_conds=[1, 1, 0]):
-    A = np.array([[A]])
-    D = np.array([[D]])
-    def x(t):
-        return initial_conds[0]*expm(A*t)[0]
+class diffuse:
 
-    def sig(t):
-        return (initial_conds[1]*expm(2*A*t) - (D/(2*A))*(1-expm(2*A*t)))[0]
+    def __init__(self, A, D, initial_conds=[1, 1, 0]):
+        self.A = np.array([[A]])
+        self.D = np.array([[D]])
+        self.funcs = [self.x, self.sig, self.broadening]
+        self.initial_conds = [1, 1, 0]
+        self.normval = None
 
-    def broadening(x, t):
-        return ((1/np.sqrt(2*np.pi*(D/2*A)*(expm(2*A*t)-1)))*expm(-0.5*((x-initial_conds[2]*expm(A*t))**2)/(D/(2*A))*(expm(2*A*t - 1))))[0]
-    return [x, sig, broadening]
+    def x(self, t):
+        return self.initial_conds[0]*expm(self.A*t)[0]
 
+    def sig(self, t):
+        return (self.initial_conds[1]*expm(2*self.A*t) - (self.D/(2*self.A))*(1-expm(2*self.A*t)))[0]
 
-funcs = diffuse(0.1, 0.1)
+    def broadening(self, x, t):
+        def unnormalised(x, t):
+            return ((1/np.sqrt(2*np.pi*(self.D/2*self.A)*(expm(2*self.A*t)-1)))*expm(-0.5*((x-self.initial_conds[2]*expm(self.A*t))**2)/(self.D/(2*self.A))*(expm(2*self.A*t - 1))))[0]
+        if not self.normval: self.normval = integr(unnormalised, -Inf, Inf, args=(t))
+        return unnormalised(x, t)/self.normval[0]
+
+funcs = diffuse(0.1, 0.1).funcs
 x = funcs[0]
 sig = funcs[1]
 broadening = funcs[2]
-T = np.linspace(-10, 10, 100)
-X = [x(t) for t in T]
-Y = [sig(t) for t in T]
-# Z = [broadening(x, 5) for x in X]
-q, = plt.plot(T, X)
-p, = plt.plot(T, Y)
-# o, = plt.plot(X, Z)
-plt.legend([q, p], ["$x$", "$sig^2$"])
+X = np.linspace(-3, 3, 100)
+# X = [x(t) for t in T]
+# Y = [sig(t) for t in T]
+Z = [broadening(x, 5) for x in X]
+# q, = plt.plot(T, X)
+# p, = plt.plot(T, Y)
+o, = plt.plot(X, Z)
+# plt.legend([q, p], ["$x$", "$sig^2$"])
 plt.xlabel("Time")
 plt.ylabel("Value")
 plt.show()
+#
