f15bbb0714077466b0d388611ea176f9d5015d95
Fergus Barratt <fergus.barratt.12@ucl.ac.uk>
2016-02-29 20:18:48 +0000

    dispersive stuff

--- Python/dispersive_plot.py
+++ Python/dispersive_plot.py
@@ -1,6 +1,5 @@
 '''Produce dispersive plot'''
 
-
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 import seaborn as sb
@@ -16,7 +15,7 @@ import quantumoptics as qo
 qo = importlib.reload(qo)
 
 ### Parameters
-matrix_size = 60 
+matrix_size = 85 
 
 # Dispersive
 c_q_det = (8.1831-10.5665)
@@ -37,33 +36,23 @@ def disp_drive(A, omega_cavity, omega_drive, sigmaz, det, g, kappa):
   return np.sqrt(A**2 * (1/omega_cavity**2)*(
             (omega_drive**2-(omega_cavity-chi(A))**2)**2 + kappa_disp**2*omega_drive**2))
 
-# Resonant
-kappa=1
-g=10
-
-def res_drive(alphasq, detuning, kappa, g):
-    return abs(-1j*np.sqrt(alphasq)*(
-            kappa - 1j*(detuning + (detuning/abs(detuning))*g**2/np.sqrt(detuning**2 + 4 * g**2 * alphasq))))
-
-
-# Q Function Method
-def abs_cavity_field_Q(sys, xvec=np.linspace(-5, 5, 50), yvec=np.linspace(-5, 5, 50)):
-    Qs = sys.qps(xvec, yvec)
-    return np.array([abs(np.sum(xvec.T.dot(Q))+1j*np.sum(Q.dot(yvec))) for Q in Qs])/34
-
 ## Semiclassical 
+
 print("Setup done")
+
 # Quantum drives (declared here to call for levels in semiclassical)
-q_disp_drives = np.linspace(0.015, 0.025, 3)
+q_disp_drives = np.linspace(0.015, 0.025, 5)
+
+# Make all the axes
 disp_fig = plt.figure()
-
-gs = gridspec.GridSpec(6, 3)
+gs = gridspec.GridSpec(6, 12)
 disp_ax = [None, None, None]
-disp_ax[0] = disp_fig.add_subplot(gs[0:2, :])
-disp_ax[1] = disp_fig.add_subplot(gs[2:4, :], sharex=disp_ax[0])
-disp_ax[2] = disp_fig.add_subplot(gs[4:6, :], sharex=disp_ax[1])
-# disp_fig, disp_ax = plt.subplots(2, sharex=True, figsize=(15, 15))
+disp_ax[0] = disp_fig.add_subplot(gs[0:2, :-1])
+disp_ax[1] = disp_fig.add_subplot(gs[2:4, :-1], sharex=disp_ax[0])
+disp_ax[2] = disp_fig.add_subplot(gs[4:6, :-1], sharex=disp_ax[1])
+cbar_ax = disp_fig.add_subplot(gs[0:6, -1:])
 
+# determine drives from A
 disp_alphas = np.linspace(0, 13, 200)
 disp_detrange = np.linspace(0.01, 0.045, 280)
 disp_drives = np.array([[disp_drive(A, 
@@ -72,62 +61,90 @@ disp_drives = np.array([[disp_drive(A,
                   -1, 
                   c_q_det, 
                   coupling_strength, 
-                  kappa_disp) for d in disp_detrange] for A in disp_alphas])
+                  kappa_disp) for d in disp_detrange]
+                              for A in disp_alphas])
 
+# Plot contours of constant drive at chosen levels
 mble_disp = disp_ax[0].contour(disp_detrange, disp_alphas, disp_drives, 
                                levels=q_disp_drives, 
                                linewidths=1.0,
-                               cmap='viridis')
-disp_ax[0].set_title('Semiclassical Dispersive', loc='right', 
-                fontdict={'fontsize': 16, 'verticalalignment': 'bottom'})
+                               cmap='jet')
+
+# Set plot parameters
+disp_ax[0].set_title('Semiclassical', 
+                     loc='right', 
+                     fontdict={'fontsize': 12, 
+                               'verticalalignment': 'bottom'})
 disp_ax[0].set_xlabel('$\omega_c - \omega_d$')
 disp_ax[0].set_ylabel('$\left|A\\right|^2$')
+
+
 if len(q_disp_drives) > 1:
-    disp_cbar = plt.colorbar(mble_disp, ax=disp_ax[2], 
-                 label='Drive Strength', 
-                 orientation='horizontal')
-    # Puts chosen set of level lines on the colorbar
-    Mark_Colorbar = False
-    if Mark_Colorbar:
-        cont = ax[0].contour(mble_disp, levels=[q_disp_drives_1, q_disp_drives_2], colors=['r', 'b'])
-        cbar.add_lines(cont, erase=False)
+    disp_cbar = plt.colorbar(mble_disp, cax=cbar_ax, 
+                 label='Drive Strength')
+                 
 
     # Wider colorbar lines
     disp_childs = disp_cbar.ax.get_children()
     disp_childs[0].set_linewidths(20)
+    
 print("Semiclassical done")
+
 ## Quantum
 # set parameters for each q_disp_drive
 q_disp_detrange = disp_detrange
-bishop_parameters = [qo.JaynesCummingsParameters(coupling_strength, matrix_size).det_params(
-                              drive_strengths=q_disp_drive, # should have xiC1 for dip @ 6.3 drive
-                              drive_cavity_detunings=q_disp_detrange, # Should have * chi0 for dip @ 6.3 drive
-                              qubit_cavity_detunings=c_q_det,
-                              c_op_params=[kappa_disp],
-                              omega_cavity=cavity_freq) for q_disp_drive in q_disp_drives]
+bishop_parameters = [qo.JaynesCummingsParameters(
+    coupling_strength, 
+    matrix_size).det_params(
+                            drive_strengths=q_disp_drive, 
+                            drive_cavity_detunings=q_disp_detrange,
+                            qubit_cavity_detunings=c_q_det,
+                            c_op_params=[kappa_disp],
+                            omega_cavity=cavity_freq) 
+                     for q_disp_drive in q_disp_drives]
 
 # Build system for each q_disp_drive
-bishop_systems = [qo.SteadyStateJaynesCummingsModel(*b_params, noisy=True) for b_params in bishop_parameters]
+bishop_systems = [qo.SteadyStateJaynesCummingsModel(*b_params,
+                                                    noisy=True)
+                  for b_params in bishop_parameters]
+
 print("\nquantum setup done")
 
 # Plot all absolute cavity fields with colors from semiclassical contours
 for sys in enumerate(bishop_systems):
     print('{} / {}'.format(sys[0]+1, len(bishop_systems)))
-    disp_ax[1].plot(q_disp_detrange, sys[1].abs_cavity_field(), linewidth=1.0, c=disp_childs[0].get_colors()[sys[0]])
+    disp_ax[1].plot(q_disp_detrange, sys[1].abs_cavity_field(),
+                linewidth=1.0, 
+                c=disp_childs[0].get_colors()[sys[0]])
 
 # disp_ax[1].plot(q_disp_detrange, abs_cavity_field_Q(bishop_system)**2, linewidth=1.0)
-disp_ax[1].set_title('Quantum Dispersive', loc='right', fontdict={'fontsize': 16, 'verticalalignment': 'bottom'})
+disp_ax[1].set_title('Quantum', 
+                     loc='right', 
+                     fontdict={'fontsize': 12, 
+                     'verticalalignment': 'bottom'})
 disp_ax[1].set_xlabel('$\omega_c - \omega_d$')
 disp_ax[1].set_ylabel('$\left | \langle a \\rangle \\right|$')
-gs.update(wspace=0.5, hspace=1)
+
 print("field done")
 
+# Plot correlators for all systems
 for sys in enumerate(bishop_systems):
     print('{} / {}'.format(sys[0]+1, len(bishop_systems)))
-    disp_ax[2].plot(q_disp_detrange, sys[1].correlator(), linewidth=1.0, c=disp_childs[0].get_colors()[sys[0]])
+    disp_ax[2].plot(q_disp_detrange, sys[1].correlator(), 
+            linewidth=1.0, 
+            c=disp_childs[0].get_colors()[sys[0]])
+
 disp_ax[2].set_xlabel('$\omega_c-\omega_d$')
+
 disp_ax[2].set_ylabel('$\langle a \sigma_- \\rangle - \langle a  \\rangle \langle \sigma_- \\rangle$')
-disp_ax[2].set_title('Correlation', loc='right',
-fontdict={'fontsize': 16, 'verticalalignment':'bottom'})
+
+disp_ax[2].set_title('Quantum', 
+                     loc='right',
+                     fontdict={'fontsize': 12,
+                     'verticalalignment':'bottom'})
+
 print("correlator done")
+
+# Update grid spacings
+gs.update(wspace=1, hspace=1)
 plt.show()
--- Python/mc.py
+++ Python/mc.py
@@ -4,10 +4,7 @@ import matplotlib.pyplot as plt
 import quantumoptics as qo
 import seaborn as sns
 sns.set()
-
-g = 10
 N = 10
-
 c_q_det = (8.1831-10.5665)
 kappa_disp = 0.0024
 gamma = 0
@@ -15,13 +12,14 @@ coupling_strength = 0.3347
 cavity_freq = 10.5665
 sigmaz=-1
 
-bis_sys = qo.JaynesCummingsParameters(g, N).det_params(
-        drive_strengths=0.02,
+bis_sys = qo.JaynesCummingsParameters(coupling_strength, N).det_params(
+        drive_strengths=0.020,
         drive_cavity_detunings=0.028,
         qubit_cavity_detunings=c_q_det,
         c_op_params=[kappa_disp],
         omega_cavity=cavity_freq)
 
-bishop_system = qo.TimeDependentJaynesCummingsModel(*bis_sys, np.linspace(0, 100, 100))
-plt.plot(bishop_system.trajectory().expect[0])
+bishop_system = qo.TimeDependentJaynesCummingsModel(*bis_sys, np.linspace(0, 1000, 1000))
+ode_res = bishop_system.trajectory()
+plt.plot(np.abs(ode_res.expect[0]))
 plt.show()
